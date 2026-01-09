bb379893d2e2e20664f8e198d42fbe9608ffa24c
varletint <intvarlet@gmail.com>
2026-01-09 22:23:27 +0100

    vercel @500

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
-const eventRoutes = require("./routes/eventRoutes");
-const ticketRoutes = require("./routes/ticketRoutes");
-const validationRoutes = require("./routes/validationRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-const transactionRoutes = require("./routes/transactionRoutes");
-const disputeRoutes = require("./routes/disputeRoutes");
-const reconciliationRoutes = require("./routes/reconciliationRoutes");
-const auditRoutes = require("./routes/auditRoutes");
-const organizerRoutes = require("./routes/organizerRoutes");
-const orderRoutes = require("./routes/orderRoutes");
+// const eventRoutes = require("./routes/eventRoutes");
+// const ticketRoutes = require("./routes/ticketRoutes");
+// const validationRoutes = require("./routes/validationRoutes");
+// const adminRoutes = require("./routes/adminRoutes");
+// const transactionRoutes = require("./routes/transactionRoutes");
+// const disputeRoutes = require("./routes/disputeRoutes");
+// const reconciliationRoutes = require("./routes/reconciliationRoutes");
+// const auditRoutes = require("./routes/auditRoutes");
+// const organizerRoutes = require("./routes/organizerRoutes");
+// const orderRoutes = require("./routes/orderRoutes");
 const { errorHandler, notFoundHandler } = require("./middleware/errorHandler");
 const app = express();
 
